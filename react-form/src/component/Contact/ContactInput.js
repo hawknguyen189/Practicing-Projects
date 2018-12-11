362f1eaf2da8b9fb149ea52681d3d7b15885223f
Hawk Nguyen <verynoob189@gmail.com>
2018-12-11 01:03:23 -0500

    Update contact form using controlled component concept - simple form

--- react-form/src/component/Contact/ContactInput.js
+++ react-form/src/component/Contact/ContactInput.js
@@ -1,24 +1,39 @@
 import React from "react";
-import "./ContactInput.css"
+import "./ContactInput.css";
+// import Cookies from 'universal-cookie';
+// const cookies = new Cookies();
+// cookies.set('fullName', 'HawkNg', { path: '/' });
+// console.log(cookies.get('fullName')); // Pacman
 export class ContactInput extends React.Component{
 
   render(){
     return(
       <div className="contact-form">
-        <form  className="container">
+        <form  className="container" onSubmit={this.props.handleSubmit}>
           <h2>Please do not hesitate to contact me</h2>
           <fieldset>
             <legend>Please send me your contact</legend>
             <div>
               <label htmlFor="firstName">First Name </label>
-              <input type="text" id="firstName" name="firstName" placeholder="Please update your first name here"/>
+              <input type="text" id="firstName" name="firstName"
+                placeholder="Please input here" onChange={this.props.handleChange}/>
             </div>
             <div>
               <label htmlFor="lastName">Last Name </label>
-              <input type="text" id="lastName" name="lastName" placeholder="Please update your last name here"/>
+              <input type="text" id="lastName" name="lastName"
+                placeholder="Please input here" onChange={this.props.handleChange}/>
+            </div>
+            <div>
+              <label htmlFor="email">Email </label>
+              <input type="text" id="email" name="email"
+                placeholder="Please input here" onChange={this.props.handleChange}/>
+            </div>
+            <div className="submit-button">
+              <input type="submit" disabled={this.props.error}/>
             </div>
           </fieldset>
         </form>
+        {this.props.error && this.props.value ? <h3>{this.props.error}</h3> : null}
       </div>
     )
   }
--- react-form/src/container/ContactContainer.js
+++ react-form/src/container/ContactContainer.js
@@ -2,10 +2,75 @@ import React from "react";
 import {ContactInput} from "../component/Contact/ContactInput"
 
 export class ContactContainer extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      error:null,
+      value:null
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleError = this.handleError.bind(this);
+  }
+
+  componentDidMount(){
+    this.setState({
+      error:this.handleError("","email"),
+      value:null
+    })
+  }
+
+  handleSubmit(e){
+    e.preventDefault()
+    console.log({target: e.target})
+    const formNodes = e.target;
+    for (let node of formNodes) {
+      if(node.nodeName.toUpperCase() === "INPUT"){
+        if(node.name === "firstName"){
+          console.log(node.value);
+        }
+      }
+    }
+  }
+
+  handleChange(e){
+    console.log({target: e.target})
+    const value = e.target.value;
+    const labelName = document.querySelector(`label[for=${e.target.id}]`).textContent
+    const elementName = e.target.name;
+    if (value){
+      let error = this.handleError(value,elementName);
+      if (error) {
+        error = error + " " + labelName;
+      }
+      this.setState({
+        error: error,
+        value:value
+      })
+    }
+  }
+
+  handleError(value,name){
+    const emailRegex = /^\w+@\w+[.][a-zA-Z]+$/g;
+    if (name ==="firstName" || name === "lastName"){
+      if(value.search(/[^a-z\s]/i)!==-1){
+        return "Your input is invalid for your"
+      } else {
+        return null
+      }
+    } else if (name === "email") {
+      if(!emailRegex.test(value)){
+        return "Your input is invalid for your"
+      } else {
+        return null
+      }
+    }
+  }
 
   render () {
     return (
-      <ContactInput/>
+      <ContactInput error={this.state.error} value={this.state.value} handleError={this.handleError}
+        handleChange={this.handleChange } handleSubmit={this.handleSubmit}/>
     )
   }
 }
