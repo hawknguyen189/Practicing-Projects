cc03d767c204ad862a266939a171fae2ef84add3
Hawk Nguyen <verynoob189@gmail.com>
2019-02-19 17:23:12 -0500

    learning nodejs + sql

--- hello web dev world/java.js
+++ hello web dev world/java.js
@@ -1,4 +1,13 @@
 var inputs = document.getElementById("my-form").elements;
 var inputByIndex = inputs[0];
 var inputByName = inputs["username"];
-alert(`${inputByName.value} new`);
+// alert(`${inputByName.value} new`);
+if (0==="0") {
+    console.log("1st true");
+}
+if (0===[]) {
+    console.log("2nd true");
+}
+if ("0"===[]) {
+    console.log("3rd true");
+}
