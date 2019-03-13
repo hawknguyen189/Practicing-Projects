991dcbb9dceb6cf05d0137a2f3b1c1023a81ce13
Hawk Nguyen <verynoob189@gmail.com>
2019-03-13 19:50:12 -0400

    reviewing JS

--- masterizing-jest-promise/public/test.js
+++ masterizing-jest-promise/public/test.js
@@ -1,1 +1,19 @@
-console.log("t4esting");
+class Person {
+    constructor(name) {
+        this.name = name
+    }
+    hello() {
+        return 'Hello, I am ' + this.name + '.'
+    }
+}
+class Programmer extends Person {
+    constructor(name,age) {
+        super(name)
+        this.age=age
+    }
+    hello() {
+        return super.hello() + ' I am a programmer.'
+    }
+}
+const hawk = new Programmer('Hawk')
+console.log(hawk.hello());
