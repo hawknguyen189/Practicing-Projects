ee8086e6ff4ef23975e8cb6a2d2e1305d61fa506
Hawk Nguyen <verynoob189@gmail.com>
2019-03-14 22:46:22 -0400

    update practicing

--- masterizing-jest-promise/public/test.js
+++ masterizing-jest-promise/public/test.js
@@ -16,4 +16,9 @@ class Programmer extends Person {
     }
 }
 const hawk = new Programmer('Hawk')
-console.log(hawk.hello());
+console.log(1);
+setTimeout(()=>console.log(2),0);
+console.log(3);
+console.log(4);
+setTimeout(()=>console.log(5),0);
+//output: 1 3 2
