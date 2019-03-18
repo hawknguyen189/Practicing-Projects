2b5aa00ead57f058251561c2723bb10969e4a9a8
Hawk Nguyen <verynoob189@gmail.com>
2019-03-18 16:09:30 -0400

    reviewing promise

--- masterizing-jest-promise/public/test.js
+++ masterizing-jest-promise/public/test.js
@@ -19,10 +19,23 @@ const hawk = new Programmer('Hawk')
 console.log(1);
 setTimeout(()=>console.log(2),0);
 console.log(3);
-console.log(4);
 setTimeout(()=>console.log(5),0);
 //output: 1 3 2
 const bar = {name:"hawk", age:28};
 const foo = {name:"kiwi", age:"8months"};
 const test = bar + foo;
 console.log(typeof test); //string
+
+const doSomethingAsync = () => {
+    return new Promise(resolve => {
+        resolve('I did something')
+    })
+}
+console.log("before call")
+const callFunc = async () => {
+    console.log(await doSomethingAsync());
+}
+console.log("just call")
+callFunc();
+console.log(4);
+console.log("after call")
