a40c0cd2f946ee8fab3f2dbd5aa8fae06715e7db
Hawk Nguyen <verynoob189@gmail.com>
2019-04-08 13:46:33 -0400

    complete setting up server & client

--- setting-up-nodejs/client/src/App.js
+++ setting-up-nodejs/client/src/App.js
@@ -1,6 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// const xhr = new XMLHttpRequest(); //create new obj
+// xhr.open("POST",
+//   "https://localhost:3001/?url=https://maximum.blog/@shalvah/posts"); // assuming you’re hosting it locally
+// xhr.setRequestHeader("Content-type", "application/json");
+// let data = {
+//   headers: {
+//     Accept: "application/json",
+//     Origin: "http://maximum.blog"
+//   },
+//   method: 'GET'
+// };
+// xhr.send(JSON.stringify(data));
+// console.log(xhr);
+const getSuggestions = async () => {
+  // const wordQuery = inputField.value;
+  const endpoint = "http://localhost:3000/ural";
+  try {
+    const response = await fetch(endpoint);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+      //return jsonResponse;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const shortenUrl = async () => {
+  const data = JSON.stringify({ user: "hawk", password:"kiwi189" });
+  try {
+    const response = await fetch("http://localhost:3000/login", {
+      method: "POST",
+      body: data,
+      headers: {
+        'Content-type': 'application/json'}
+    })
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      console.log("resoonse is not ok",jsonResponse);
+    };
+  } catch (error) {
+    console.log("fetch fail",error);
+  }
+};
+
+getSuggestions();
+// shortenUrl();
 
 class App extends Component {
   render() {
---
+++ setting-up-nodejs/server/app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.get("/url", (req, res, next) => {
+  console.log("here is the get response");
+  res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+});
+app.post('/login', (req, res, next) => {
+  console.log(req.body);
+  var user_name = req.body.user;
+  var password = req.body.password;
+  console.log("User name = " + user_name + ", password is " + password);
+  res.end("yes");
+});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+module.exports = app;
