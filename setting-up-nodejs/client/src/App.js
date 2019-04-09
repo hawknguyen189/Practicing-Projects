ff1936d41cd11d7febace71a4e0af0f32fb434a1
Hawk Nguyen <verynoob189@gmail.com>
2019-04-09 19:27:48 -0400

    set up d3

--- setting-up-nodejs/client/src/App.js
+++ setting-up-nodejs/client/src/App.js
@@ -28,11 +28,16 @@ const getSuggestions = async () => {
     console.log(error);
   }
 };
-
+const url = "https://trends.google.com/trends/api/explore?hl=en-US&req=%7B%22comparisonItem%22%3A%5B%7B%22keyword%22%3A%22bitcoin%22%2C%22hl%22%3A%22en-US%22%2C%22category%22%3A0%2C%22timezone%22%3A240%2C%22property%22%3A%22%22%2C%22endTime%22%3A%222019-04-08T19%3A34%3A11.964Z%22%2C%22startTime%22%3A%222004-01-01T00%3A00%3A00.000Z%22%2C%22time%22%3A%222004-01-1%202019-04-8%22%7D%5D%2C%22category%22%3A0%2C%22property%22%3A%22%22%7D&tz=240"
 const shortenUrl = async () => {
-  const data = JSON.stringify({ user: "hawk", password:"kiwi189" });
+  const data = JSON.stringify({
+    headers: {
+    Accept: "application/json",
+    Origin: "http://maximum.blog"
+  },
+  method: 'GET'});
   try {
-    const response = await fetch("http://localhost:3000/login", {
+    const response = await fetch(`http://localhost:3000/api?url=${url}`, {
       method: "POST",
       body: data,
       headers: {
@@ -40,15 +45,31 @@ const shortenUrl = async () => {
     })
     if (response.ok) {
       const jsonResponse = await response.json();
-      console.log("resoonse is not ok",jsonResponse);
+      console.log("resoonse is ok",jsonResponse);
     };
   } catch (error) {
     console.log("fetch fail",error);
   }
 };
 
-getSuggestions();
+const googleTrend = async () => {
+  // const wordQuery = inputField.value;
+  const endpoint = "http://localhost:3000/trends";
+  try {
+    const response = await fetch(endpoint);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+      //return jsonResponse;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// getSuggestions();
 // shortenUrl();
+googleTrend();
 
 class App extends Component {
   render() {
--- setting-up-nodejs/server/app.js
+++ setting-up-nodejs/server/app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// var bodyParser = require('body-parser');
+let request = require("request");
+const http = require("http");
+// const url = require("url");
 
 var app = express();
 
@@ -15,7 +19,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,11 +32,82 @@ app.get("/url", (req, res, next) => {
 });
 app.post('/login', (req, res, next) => {
   console.log(req.body);
-  var user_name = req.body.user;
-  var password = req.body.password;
+  var user_name = req.body.headers;
+  var password = req.body.method;
   console.log("User name = " + user_name + ", password is " + password);
-  res.end("yes");
+  res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+  // res.end("yes");
 });
+
+
+//work as proxy server
+let getPostData = request => {
+  return new Promise((resolve, reject) => {
+    let post = "";
+    console.log("get post data",request.method);
+    if (request.method === "POST") {
+      let body = "";
+      request.on("data", data => {
+        body += data;
+      });
+      
+      request.on("end", () => {
+        post = JSON.parse(body);
+        console.log("resolve post data", post);
+        resolve(post);
+      });
+    }
+  });
+};
+app.post('/api', (req, res, next) => {
+  console.log("Server running on port ");
+  let targetUrl = req.query.url;
+  console.log(`Reuest to: ${req.query.url}`);
+  // console.log(`query is ${query}`);
+  
+  getPostData(req).then(body => {
+    console.log(body);
+    let options = {
+      uri: targetUrl,
+      headers: body.headers,
+      method: body.method || "GET"
+    };
+
+
+    let proxyCallback = (proxyErr, proxyRes, proxyBody) => {
+      if (proxyErr) {
+        console.log(proxyErr);
+        res.statusCode = 500;
+        res.write(proxyErr);
+      } else {
+        res.writeHead(proxyRes.statusCode, proxyRes.headers);
+        res.write(proxyBody);
+      }
+      res.end();
+    };
+    request(options, proxyCallback);
+  }).catch(err => { console.log("foo", err)});
+});
+
+// *******************************
+const googleTrends = require("google-trends-api");
+// import googleTrends from "./google-trends-api-master/src/index";
+
+app.get('/trends', (req, res, next) => {
+  console.log("start api requesting");
+  googleTrends
+    .interestOverTime({ keyword: "bitcoin" })
+    .then(function (results) {
+      // console.log(results);
+      res.json(results);
+    })
+    .catch(function (err) {
+      // console.error(err);
+      res.json(err);
+    });
+});
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
