1bbe262cf680a05e897f048e9c7815df768525f5
Hawk Nguyen <verynoob189@gmail.com>
2019-07-20 21:55:33 -0400

    minor update

--- setting-up-nodejs/server/app.js
+++ setting-up-nodejs/server/app.js
@@ -162,6 +162,7 @@ rp(requestOptions)
 });
 
 // catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
