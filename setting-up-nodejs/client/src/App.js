5174b67d3256b2ecf17e739656d11222ac5c31f9
Hawk Nguyen <verynoob189@gmail.com>
2019-08-05 22:16:55 -0400

    uploade alert

--- setting-up-nodejs/client/src/App.js
+++ setting-up-nodejs/client/src/App.js
@@ -133,6 +133,7 @@ const get24hVolume = async () => {
   } catch (error) {
     console.log("this is the error from cmc", error);
     console.log("this is the error from cmc", error);
+    console.log("this is the error from cmc", error);
   }
 };
 
